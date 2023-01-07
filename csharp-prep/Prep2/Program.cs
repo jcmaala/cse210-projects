2e26d61f38cc8dfa6fc7675bd634d6489049ee90
jcmaala <jcmaala27@gmail.com>
2023-01-07 09:46:48 +0800

    Finished prep 1 and prep 2 assignments.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade_num = Console.ReadLine();
+        int grade_num_fin = int.Parse(grade_num);
+
+        string letter = "";
+
+        if (grade_num_fin >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade_num_fin >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade_num_fin >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade_num_fin >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade_num_fin < 60)
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (grade_num_fin >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the subject!");
+        }
+        else
+        {
+            Console.WriteLine("It's okay. You can try again next time.");
+        }
+
     }
 }
