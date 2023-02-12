67d8356a3e9f8899f3aa3bf5add6c1b95c1a6332
jcmaala <jcmaala27@gmail.com>
2023-02-12 11:32:09 +0800

    week 6 assignment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Random randomGenerator = new Random();
+        int wordSelector = randomGenerator.Next(0, 25);
+
+        Scripture script = new Scripture();
+        Console.WriteLine($" {script.getReference()} {script.getScripture()}");
+
+        Console.WriteLine();
+        Console.WriteLine("Please enter to continue or type 'quit' to finish.");
+        string user = Console.ReadLine();
+
+        
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,16 @@
+public class Scripture
+{
+    private string  _scriptureWords = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
+    private string _reference = "John 3:16";
+    public string getScripture()
+    {
+        return _scriptureWords;
+    }
+
+    public string getReference()
+    {
+        return _reference;
+    }
+
+
+}
