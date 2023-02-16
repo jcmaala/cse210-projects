03631a936b54c5845a3bb7c8a0c8087e0a81aa53
jcmaala <jcmaala27@gmail.com>
2023-02-16 10:22:20 +0800

    Uploaded learning 04 assignment.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment as1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(as1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment newas = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3" , "Problems 8-19");
+        Console.WriteLine(newas.GetSummary());
+        Console.WriteLine(newas.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment write = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(write.GetSummary());
+        Console.WriteLine(write.GetWritingInformation());
     }
 }
